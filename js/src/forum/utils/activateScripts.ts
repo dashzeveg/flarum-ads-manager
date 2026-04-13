f2f2d118cd30baf5fa08c683d1e2f5a189b677b2
dashka <dashka.gb@gmail.com>
2026-04-13 20:21:06 +0800

    show google ads #2

--- js/src/forum/utils/activateScripts.ts
+++ js/src/forum/utils/activateScripts.ts
@@ -6,6 +6,25 @@ export default function activateScripts(container: HTMLElement) {
       newScript.setAttribute(attr.name, attr.value);
     });
     newScript.textContent = oldScript.textContent;
+
+    // Handle scripts that use document.write (like Google DCM ads)
+    if (oldScript.src) {
+      const originalWrite = document.write.bind(document);
+      document.write = (html: string) => {
+        const temp = document.createElement('div');
+        temp.innerHTML = html;
+        while (temp.firstChild) {
+          container.appendChild(temp.firstChild);
+        }
+      };
+      newScript.onload = () => {
+        document.write = originalWrite;
+      };
+      newScript.onerror = () => {
+        document.write = originalWrite;
+      };
+    }
+
     oldScript.parentNode!.replaceChild(newScript, oldScript);
   });
 }
